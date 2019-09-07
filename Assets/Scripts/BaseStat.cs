6fc2b1aeeec3a9d5dab2ccd364907db704c8d681
Aescosaurus <michaelbuslik@hotmail.com>
2019-09-06 21:19:37 -0600

    Character stats.

---
+++ Assets/Scripts/BaseStat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStat
+{
+	public BaseStat( int baseValue,string statName,
+		string statDesc )
+	{
+		this.BaseAdditives = new List<StatBonus>();
+		this.BaseValue = baseValue;
+		this.StatName = statName;
+		this.StatDesc = statDesc;
+	}
+
+	public void AddStatBonus( StatBonus toAdd )
+	{
+		this.BaseAdditives.Add( toAdd );
+	}
+
+	public void RemoveStatBonus( StatBonus toRemove )
+	{
+		this.BaseAdditives.Remove( toRemove );
+	}
+
+	public int GetCalculatedStatValue()
+	{
+		this.BaseAdditives.ForEach(
+			x => this.FinalValue += x.BonusValue );
+		FinalValue += BaseValue;
+
+		return FinalValue;
+	}
+
+	public List<StatBonus> BaseAdditives { get; set; }
+	public int BaseValue { get; set; }
+	public string StatName { get; set; }
+	public string StatDesc { get; set; }
+	public int FinalValue { get; set; }
+}
---
+++ Assets/Scripts/StatBonus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatBonus
+{
+	public StatBonus( int bonusValue )
+	{
+		this.BonusValue = bonusValue;
+
+		Debug.Log( "New stat bonus initiated." );
+	}
+
+	public int BonusValue { get; set; }
+}
